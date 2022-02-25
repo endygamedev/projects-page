3f464772bc928c23623c99f4349814e1f0a63ea3
Egor <bronnikov.40@mail.ru>
2022-02-26 02:11:06 +0300

    Remove scaling

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <title>Projects Repository | Egor Bronnikov</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" type="image/png" href="/assets/favicon.png">
   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>
   <link rel='stylesheet' href='https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap.min.css'>
